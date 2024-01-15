d143573b41fc44f5e776ea9bfafe21780e341378
SAMEER LAMA <tamansameer5@gmail.com>
2024-01-15 13:43:15 +0545

    change style attr

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//tab start
 $(".list ul li a").click(function(){
     $(".list ul li a").removeClass("active");
     $(this).addClass("active");
-    $(".tab").removeClass("active")
+    $(".tab").removeClass("active");
     $("#" + $(this).attr("data-tab")).addClass("active");
     });
+    //tab end
+    //footer click start
+    $(".fotterlist1 h3").click(function(){
+        $(".fotterlist1 h3").removeClass("active");
+        $(this).addClass("active");
+        $(".click").removeClass("active");
+        $("#" + $(this).attr("data-click")).addClass("active");
+        });
+    //footer click end
